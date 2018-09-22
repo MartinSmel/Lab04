84ab316eec3aca95ae9516d09186602e9116f142
MartinSmel <43166833+MartinSmel@users.noreply.github.com>
2018-09-22 15:55:17 +0200

    changed testthat and got rid of errors in testing

--- R/linreg.R
+++ R/linreg.R
@@ -1,15 +1,32 @@
+calculateMagicRainbowStars = function(p_value) {
+  if (p_value > 0.1) return(" ")
+  if (p_value > 0.05) return(".")
+  if (p_value > 0.01) return("*")
+  if (p_value > 0.001) return("**")
+  return("***")
+}
+cPrint = function(x, stripoff = FALSE) {
+  if (is.data.frame(x)) {
+    print(x, row.names = stripoff)
+  }
+  else {
+    print(x)
+  }
+}
+
 #create class
 linreg <- setRefClass ( "linreg",
                          fields = list  ( #lst = "list",
-                                         #form = "formula",
-                                         #data = "data.frame",
-                                         Regressions_coefficients = "numeric",
+                                         data_name = "character",
+                                         form = "formula",
+                                         data = "data.frame",
+                                         Regressions_coefficients = "vector",
                                          The_fitted_value = "matrix",
                                          The_residuals = "matrix",
                                          The_degrees_of_freedom = "numeric",
                                          The_residual_variance = "numeric",
                                          The_variance_of_the_reg_coef = "matrix",
-                                         t_values = "matrix"
+                                         t_values = "list"
                                        ) ,
                          methods = list (
                            initialize = function(form, data)
@@ -28,7 +45,9 @@ linreg <- setRefClass ( "linreg",
                              Q_1 <- Q[,c(1:n)]
                              Q_2 <- Q[,c((n+1):m)]
                              #Regression coef.
-                             beta <- as.vector(solve(R_1)%*%t(Q_1)%*%y)
+                             beta <- as.vector((solve((t(X) %*% X)) %*% t(X) %*% y))
+                             #beta <- as.vector(solve(R_1)%*%t(Q_1)%*%y)
+                             names(beta) <- c("(Intercept)", "Sepal.Width", "Sepal.Length")
                              #fitted values
                              fit_val <- X%*%beta
                              #residuals
@@ -38,11 +57,18 @@ linreg <- setRefClass ( "linreg",
                              #res. variance
                              sigma <- as.vector((t(e)%*%e)/df)
                              #variance of the reg. coef.
-                             var <- (sigma)^2 * solve(R_1%*%t(R_1)) 
+                             var <- sigma * solve(t(R)%*%R) 
+                             #var <- sigma * solve(t(as.matrix(X)) %*% as.matrix(X))
                              #%*% solve(t(R_1))
                              #t-values
-                             t <- beta/sqrt(var)
+                             t = list()
+                             for(i in (1:3))
+                             t[[i]] <- beta/sqrt(var[[i,i]])
+                            # t <- as.vector(t)
+                             .self$data_name <- deparse(substitute(data))
                           #   my_object <- linreg(
+                             .self$form <- form
+                             .self$data <- data
                              .self$Regressions_coefficients <- beta
                              .self$The_fitted_value <- fit_val
                              .self$The_residuals <- e
@@ -54,30 +80,44 @@ linreg <- setRefClass ( "linreg",
                            },                       
                           #################################### 
                           print = function (...) {
-                            x <-list()
-                             for(i in 1:length(Regressions_coefficients))
-                             x[[i]]<- Regressions_coefficients[[i]]
-                           list_names <- names(x)
-                             if (is.null(list_names)) 
-                             {
-                              list_names <- rep("", length(x))
-                             }
-                             print_element <- function(i)
-                             {
-                               if (list_names[i]=="")
-                               {
-                               cat("[[",i,"]]\n", sep="")
-                               }
-                               else
-                               {
-                                 cat("$", list_names[i], "\n", sep="")
-                                # print(x[[i]], ...)
-                               }
-                                 print(x[[i]])
-                               cat("\n")
-                             }
-                             invisible(lapply(i<-1:length(x), print_element))
+                      #      x <-list()
+                      #       for(i in 1:length(Regressions_coefficients))
+                      #       x[[i]]<- Regressions_coefficients[[i]]
+                      #     list_names <- names(x)
+                      #       if (is.null(list_names)) 
+                      #       {
+                      #        list_names <- rep("", length(x))
+                      #       }
+                      #       print_element <- function(i)
+                      #       {
+                      #         if (list_names[i]=="")
+                      #         {
+                      #         cat("[[",i,"]]\n", sep="")
+                      #         }
+                      #         else
+                      #         {
+                      #           cat("$", list_names[i], "\n", sep="")
+                      #          # print(x[[i]], ...)
+                      #         }
+                      #           cat(x[[i]])
+                      #         cat("\n")
+                      #       }
+                      #       invisible(lapply(i<-1:length(x), print_element))
+                            # Formula
+                            
+                            cat(paste("linreg(formula = ", format(form), ", data = ", data_name, ")\n\n", sep = ""))
+                            
+                            # Coefficients
+                            cat(paste("Coefficients:\n\n"))
+                            
+                            # Values
+                            table = setNames(data.frame(matrix(ncol = length(Regressions_coefficients), nrow = 0)), names(Regressions_coefficients))
+                            for (i in 1:length(Regressions_coefficients)) {
+                              table[1,i] = round(Regressions_coefficients[i], 3)
+                            }
+                              cPrint(table)
                            },
+                      
                            plot = function () {
                              #Still wrong!
                              library(ggplot2)
@@ -106,27 +146,50 @@ linreg <- setRefClass ( "linreg",
                           
                            summary = function () {
                              #######
+                           #  
+                          #   list_elements <- list(Regressions_coefficients,
+                          #                      The_variance_of_the_reg_coef,
+                          #                      t_values,
+                          #                      The_residual_variance,
+                          #                      The_degrees_of_freedom
+                          #                      )
+                          #   list_names <-c("coefficients","standard error","t-value","sigma", "degrees of freedom")
+                          #   p_element <- function(i)
+                          #   {
+                          #     cat("$", list_names[i], "\n", sep="" )
+                          #     cat(list_elements[[i]], show_b = TRUE)
+                          #     cat("\n")
+                             "Prints a summary of the Linear Regression"
                              
-                             list_elements <- list(Regressions_coefficients,
-                                                The_variance_of_the_reg_coef,
-                                                t_values,
-                                                The_residual_variance,
-                                                The_degrees_of_freedom
-                                                )
-                             list_names <-c("coefficients","standard error","t-value","sigma", "degrees of freedom")
-                             p_element <- function(i)
-                             {
-                               cat("$", list_names[i], "\n", sep="" )
-                               print(list_elements[[i]], show_b = TRUE)
-                               cat("\n")
+                             cat("\nCall:\n")
+                             cat(paste("linreg(formula = ", (format(form)), ", data = ", data_name, ")\n\n", sep = ""))
+                             
+                             # Coefficients
+                             cat("Coefficients:\n\n")
+                             
+                             # Values
+                             table = data.frame(matrix(ncol = 5, nrow = 0))
+                             for (i in 1:length(Regressions_coefficients)) {
+                               # Beta (coefficients), std error, t values, p values
+                               local_t_value = Regressions_coefficients[i]/sqrt(The_variance_of_the_reg_coef[i, i])
+                               local_p_value = 2 * pt(abs(local_t_value), The_degrees_of_freedom, lower.tail = FALSE)
+                               newRow = data.frame(round(Regressions_coefficients[i], 2), round(sqrt(The_variance_of_the_reg_coef[i, i]), 2), round(local_t_value, 2), formatC(local_p_value, format = "e", digits = 2), calculateMagicRainbowStars(local_p_value))
+                               rownames(newRow)[1] = rownames(The_variance_of_the_reg_coef)[i]
+                               table = rbind(table, newRow)
                              }
-                             invisible(lapply(i<-1:5, p_element))
-                           }
+                             
+                             colnames(table) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)", "")
+                             cPrint(table, TRUE)
+                             cat(paste("\nResidual standard error:", sqrt(The_residual_variance), "on", The_degrees_of_freedom, "degrees of freedom"))
+                             
+                             }
+                            # invisible(lapply(i<-1:5, p_element))
+                           #}
                          )
 )
 
 
-#object = linreg$new(form = Petal.Length~Sepal.Width+Sepal.Length, data = iris)
+object = linreg$new(form = Petal.Length~Sepal.Width+Sepal.Length, data = iris)
 #print.list(object$data, show_b = TRUE)
 #plot(object)
 #resid(object)
@@ -138,3 +201,4 @@ linreg <- setRefClass ( "linreg",
 ###################
 
 
+ 
